c867021eb9ae28367432b408de134bf4f1a3567b
Raunak Raj <145150966+MeRAUNAK@users.noreply.github.com>
2023-12-05 12:22:32 +0530

    Add files via upload

---
+++ area math function.py
@@ -0,0 +1,13 @@
+import math
+r=float(input("enter radius"))
+h=math.pi
+area=r*r*h
+print("%.2f"%area)
+print("%.4f"%area)
+
+a=float(input("enter radius"))
+b=math.pi
+ar1=a*a*b
+print("%.2f"%ar1)
+print("area of circle= ",f" {ar1:.4f}")
+
